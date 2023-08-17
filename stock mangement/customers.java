8e216ef190f307e932a2a6a99831640506c4d17e
Jayashree1153 <113369969+Jayashree1153@users.noreply.github.com>
2023-08-17 11:02:41 +0530

    Add files via upload

---
+++ stock mangement/customers.java
@@ -0,0 +1,55 @@
+
+
+/**
+ * Class customers
+ */
+public class customers {
+
+  //
+  // Fields
+  //
+
+  private void place_order;
+  
+  //
+  // Constructors
+  //
+  public customers () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of place_order
+   * @param newVar the new value of place_order
+   */
+  private void setPlace_order (void newVar) {
+    place_order = newVar;
+  }
+
+  /**
+   * Get the value of place_order
+   * @return the value of place_order
+   */
+  private void getPlace_order () {
+    return place_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+}
